9023483655ae627080a1b0d6536efccecb75196e
somensomes <somensomes@DESKTOP-8CB2P0K>
2020-09-23 19:56:15 +0900

    스레드를 이용한 마우스 좌표값 추출

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace JGJ_Thread_Mouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelx = new System.Windows.Forms.Label();
+            this.labely = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("넥슨Lv1고딕 OTF Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(80, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X 좌표 값 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("넥슨Lv1고딕 OTF Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(242, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Y 좌표 값 :";
+            // 
+            // labelx
+            // 
+            this.labelx.AutoSize = true;
+            this.labelx.Font = new System.Drawing.Font("넥슨Lv1고딕 OTF", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelx.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.labelx.Location = new System.Drawing.Point(156, 92);
+            this.labelx.Name = "labelx";
+            this.labelx.Size = new System.Drawing.Size(57, 18);
+            this.labelx.TabIndex = 0;
+            this.labelx.Text = "label1";
+            // 
+            // labely
+            // 
+            this.labely.AutoSize = true;
+            this.labely.Font = new System.Drawing.Font("넥슨Lv1고딕 OTF", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labely.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.labely.Location = new System.Drawing.Point(318, 92);
+            this.labely.Name = "labely";
+            this.labely.Size = new System.Drawing.Size(57, 18);
+            this.labely.TabIndex = 0;
+            this.labely.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("넥슨Lv1고딕 OTF Bold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(353, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Thread 처리하여 마우스 좌표값 추출";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("넥슨Lv1고딕 OTF Bold", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(501, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Thread &중지";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("넥슨Lv1고딕 OTF Bold", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(501, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Thread &재시작";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(703, 141);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labely);
+            this.Controls.Add(this.labelx);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JGJ_Thread_Mouse";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label labely;
+        public System.Windows.Forms.Label labelx;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace JGJ_Thread_Mouse
+{
+    public partial class Form1 : Form
+    {
+        //스레드 생성 MouseThread
+        Thread MouseThread; 
+        public Form1()
+        {
+
+            InitializeComponent();
+            //위에서 생성한 스레드에 ThreadMouse 함수 사용
+            MouseThread = new Thread(ThreadMouse); 
+            MouseThread.Start(); 
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ThreadMouse()
+        {
+            while (true)
+            { 
+            int x = MousePosition.X;
+            int y = MousePosition.Y;
+
+            // 생성되어진 스레드가 아니고 다른 스레드에서 호출된경우 true반환 -> 스레드생성
+            if (this.InvokeRequired) 
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                        labelx.Text = x.ToString();
+                        labely.Text = y.ToString();
+ 
+                }
+                ));          
+            }
+                // InvokeRequired false 경우 정상 작업진행
+                else
+                {
+                    labelx.Text = x.ToString();
+                    labely.Text = y.ToString();
+                }
+             }
+        }
+
+        // 스레드 중단 버튼 이벤트
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MouseThread.Abort();
+        
+        }
+
+        // 스레드 재할당 버튼 이벤트
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 스레드를 중단하게되면 (Abort) 스레드를 재할당해야한다. 
+            MouseThread = new Thread(new ThreadStart(ThreadMouse));
+            MouseThread.Start();
+        }
+    }
+}
